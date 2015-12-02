16846a2d590ad17cc8d0cd5575fd3cba3baf765a
Arya Asemanfar <arya.asemanfar@mixpanel.com>
2015-12-02 05:12:37 +0000

    Add git sha and branch to go binaries.
    Report the metric 'arb.remap.git_version' to wavefront. The numeric value in wavefront
    won't be useful in and of itself, but you can query to see how many different versions
    are running, and which servers are running different versions.

--- defaults.go
+++ defaults.go
@@ -3,6 +3,8 @@ package obs
 import (
 	"obs/logging"
 	"obs/metrics"
+	"time"
+	"version"
 )
 
 var Log logging.Logger
@@ -16,4 +18,13 @@ func init() {
 	} else {
 		Metrics = metricsReceiver
 	}
+}
+
+func ReportVersion(receiver metrics.Receiver) {
+	go func() {
+		for {
+			receiver.SetGauge("git_version", float64(version.Int()))
+			time.Sleep(60 * time.Second)
+		}
+	}()
 }
