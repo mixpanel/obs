fec7d73e5e3de6fd05cc7a3fe33ffa2fd7086c89
Arya Asemanfar <arya.asemanfar@mixpanel.com>
2016-07-27 20:25:24 +0000

    Storage-server: more tests
    Change-Id: Ifd0e08660e310dad3bb5e77c8cf47e62caa0b5f7

--- logging/formatter.go
+++ logging/formatter.go
@@ -21,7 +21,8 @@ func jsonFormatter(lvl level, name, message string, fields Fields) string {
 	fields.Update(localhostFields)
 	delete(fields, "hostname") // added automatically
 	fields["logger"] = name
-	fields["level"] = levelToString(lvl)
+	fields["level"] = levelToString(lvl) // TOOD: remove once we're entirely on GCP
+	fields["severity"] = levelToString(lvl)
 	fields["message"] = message
 
 	formatted, err := json.Marshal(fields)
